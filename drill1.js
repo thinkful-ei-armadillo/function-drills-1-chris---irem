9656e4aae2efe16f0c50b5ac03a6abe01953032b
Christopher Carnivale <christopher.carnivale@gmail.com>
2019-01-14 16:07:08 -0500

    Year of birth function

--- drill1.js
+++ drill1.js
@@ -1,7 +1,12 @@
 'use strict';
+function getYearOfBirth(age){
+  const yearOfBirth = 2016 - age;
+  return yearOfBirth;  
+}
 function createGreeting(name, age){ 
-  const yearOfBirth = 2016 - age; 
+  const yearOfBirth = getYearOfBirth(age);  
   return 'Hi, my name is Chris and I\'m 35 years old.' `I was born in ${yearOfBirth}.`;
 }
-const greeting1 = createGreeting();
+
+const greeting1 = createGreeting('Chris', 35);
 console.log(greeting1); 
