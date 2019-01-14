70594cc2af68bd5fbcb038c378f2155d603b0f8d
Christopher Carnivale <christopher.carnivale@gmail.com>
2019-01-14 16:42:02 -0500

    More error handling

--- drill1.js
+++ drill1.js
@@ -6,12 +6,18 @@ function getYearOfBirth(age){
 function createGreeting(name, age){ 
   if(age <= 0) {
     throw new Error('Age can not be negative.');
+  }
+  if(name === undefined || age === undefined){
+    throw new Error('Arguments not valid'); 
+  }
+  if(typeof name !== 'string' || typeof age !== 'number'){
+    throw new TypeError(); 
   }  
   const yearOfBirth = getYearOfBirth(age);  
   return 'Hi, my name is Chris and I\'m 35 years old.' `I was born in ${yearOfBirth}.`;
 }
 try {
-  const greeting1 = createGreeting('Chris', 35);
+  const greeting1 = createGreeting(35, 35);
   console.log(greeting1);
 } 
 catch (e) {
