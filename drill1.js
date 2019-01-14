2eb16366ab8b060ead1707aa57d96c86616c52f9
Christopher Carnivale <christopher.carnivale@gmail.com>
2019-01-14 15:53:18 -0500

    Saved value

--- drill1.js
+++ drill1.js
@@ -1,5 +1,6 @@
 'use strict';
 function createGreeting(){ 
-  console.log('Hi, my name is Chris and I\'m 35 years old.');
+  return 'Hi, my name is Chris and I\'m 35 years old.';
 }
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1); 
