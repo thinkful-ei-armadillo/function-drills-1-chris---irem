fdc707b171657baeb0072b5f50195cf0f910a247
Irem Sen <secilreel@yahoo.com>
2019-01-14 17:30:13 -0500

    Infinity and Beyond

--- drills2.js
+++ drills2.js
@@ -1,7 +1,22 @@
 'use strict'; 
 function jediName(firstName,lastName){
-  let jediFirst = firstName.slice(0,2); 
-  let jediLast = lastName.slice(0,1);  
+  let jediFirst = firstName.slice(0,3); 
+  let jediLast = lastName.slice(0,2);  
   return jediLast + jediFirst;
 }
-jediName('chris', 'irem');
+console.log(jediName('chris', 'irem'));
+function beyond(num){
+  if (num === Infinity){
+    console.log('and beyond');  
+  }
+  else if (num>0){
+    console.log('to infinity');
+  }
+  else if (num<0){
+    console.log('to negative infinity');
+  }
+  else {
+    console.log('staying home');
+  }
+}
+console.log(beyond(Infinity));
